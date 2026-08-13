8ace274044857578926e631a5f45b279819e14dd
Kerem Kayalar <kayalar.kerem@gmail.com>
2026-08-13 18:40:38 +0300

    chore(android): bump Play build to version code 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -299,7 +299,7 @@ android {
         applicationId = "com.riskdetectedan.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
